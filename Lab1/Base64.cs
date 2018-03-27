1840e7882b3ba890c24f63d4162560e0dcde47db
lockystocky <locky.stocky24@gmail.com>
2018-03-27 12:22:52 +0300

    Initial commit

---
+++ Lab1/Base64.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64
+{
+
+    public class Base32Converter
+    {
+        public static string ToBase32(string input)
+        {
+            string base64arr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+            string base64Str = "";
+            int i;
+           
+            string binaryStr = input.Select(s => BinaryConverter.ToBinary(s)).Aggregate((k, l) => k + l);
+            
+            for (i = 0; i < (binaryStr.Length - 5); i += 6)
+            {
+                int symbNumb = Convert.ToInt32(binaryStr.Substring(i, 6), 2);
+                base64Str += base64arr[symbNumb];
+            }
+
+            if(i < (binaryStr.Length - 1))
+            {
+                string s = binaryStr.Substring(i);
+                s += Enumerable.Repeat("0", 6 - s.Length).Aggregate((k, l) => k + l);
+                int ss = Convert.ToInt32(s, 2);
+                base64Str += base64arr[ss];
+            }
+
+            base64Str += Enumerable.Repeat("=", binaryStr.Length % 3).Aggregate((k, l) => k + l);
+
+            return base64Str;
+
+        }
+
+        public static void ToBase32(string inputPath, string outputPath)
+        {
+            string simpleText = File.ReadAllText(inputPath, Encoding.Default);
+            var simpleTextBytes = Encoding.UTF8.GetBytes(simpleText);
+            string encodedText = Convert.ToBase64String(simpleTextBytes);
+            File.WriteAllText(outputPath, encodedText);
+        }
+    }
+
+    public class BinaryConverter
+    {
+        public static string ToBinary(int symbol)
+        {
+            int k = 128;
+            string binaryStr = "";
+            while (k > 0)
+            {
+                binaryStr += Convert.ToInt32((k & symbol) != 0);
+                k = k >> 1;
+            }
+            return binaryStr;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Base32Converter.ToBase32("D:\\entropyPCI.txt.bz2", "D:\\entropyPCIbz2.txt");
+
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/InfoAmount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entropy
+{
+    public class InfoResearch
+    {
+        public static double GetSymbolFrequency(char symbol, string filePath)
+        {
+            string text = File.ReadAllText(filePath, Encoding.Default);
+            double symbCount = text.Where(s => s == symbol).Count();
+            int fileSymbCount = text.Count();
+            if (fileSymbCount == 0) return 0;
+            return (symbCount / fileSymbCount);
+        }
+
+        public static Dictionary<char, double> GetSymbolsFrequencies(string filePath)
+        {
+            Dictionary<char, double> frequencies = new Dictionary<char, double>();
+            string text = File.ReadAllText(filePath, Encoding.Default);
+
+            foreach (var symbol in text)
+            {
+                if (!frequencies.ContainsKey(symbol))
+                {
+                    frequencies.Add(symbol, GetSymbolFrequency(symbol, filePath));
+                }
+            }
+
+            return frequencies;
+        }
+
+        public static double GetAvgEntropy(string filePath)
+        {
+            var symbols = File.ReadAllText(filePath, Encoding.Default).Distinct();
+            double entropy = 0;
+            foreach (var symbol in symbols)
+            {
+                double frequency = GetSymbolFrequency(symbol, filePath);
+               if(frequency != 0) entropy += frequency * Math.Log(frequency, 2);
+            }
+            entropy = -entropy;
+            return entropy;
+        }
+
+        public static double GetInfoAmount(string filePath)
+        {
+            var symbolsCount = File.ReadAllText(filePath, Encoding.Default).Count();
+                
+            return symbolsCount * GetAvgEntropy(filePath) / 8;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\entropyPCIbz2.txt";
+            var frequencies = InfoResearch.GetSymbolsFrequencies(filePath);
+            foreach(var frequency in frequencies)
+            {
+                Console.WriteLine("{0} : {1}", frequency.Key, frequency.Value);
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Entropy (bits): {0}", InfoResearch.GetAvgEntropy(filePath));
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Amount of information (bytes): {0}", InfoResearch.GetInfoAmount(filePath));
+            Console.WriteLine("--------------------");
+            Console.WriteLine("File size (bytes): {0}", new FileInfo(filePath).Length);
+
+            Console.ReadKey();
+
+        }
+    }
+}
